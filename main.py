b89683ffce6a0463f1890188f8a4513fb6b4b568
Eyad. <160641611+IiTRoFxDev@users.noreply.github.com>
2024-11-15 14:37:55 +0200

    Update main.py
    added standard deviation

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-class measures_of_center :
+class measures_of_center_and_spread :
     def __init__(self , numbers=[]):
         self.numbers = numbers
         print("coded by Eyad. (iitrofxdev)")
     def mean(self):
         if len(self.numbers) == 0:
-            return "there isnot any number"
-        return f"the mean = {sum(self.numbers) / len(self.numbers)}"
+            raise ValueError
+        return  sum(self.numbers) / len(self.numbers)
     def median(self):
         self.numbers.sort()
         if len(self.numbers) == 0:
-            return "there isnot any number"
+            raise ValueError
         first_number = int((len(self.numbers)+1)/ 2)-1
         if (len(self.numbers)+1) % 2:
-           return f"the median = {sum(self.numbers[first_number:first_number+2]) / 2}"
-        return f"the median = {self.numbers[first_number]}"    
+           return {sum(self.numbers[first_number:first_number+2]) / 2}
+        return {self.numbers[first_number]}
     def mode(self):
         frequency = {}
         for i in self.numbers:
@@ -26,10 +26,16 @@ class measures_of_center :
         for key , value in frequency.items():
             if frequency[key] == the_value:
                 end.append(key)
-        return f"the mode = {end}"
-
-data = measures_of_center()
+        return  end
+    def standered_deviation(self):
+        sigma = 0
+        x_dash = data.mean()
+        for item in self.numbers:
+            sigma+=((item - x_dash)**2)
+        return [((sigma / (len(self.numbers)-1))**0.5) , ((sigma / (len(self.numbers)))**0.5)]
+data = measures_of_center_and_spread()
 data.numbers = [int(i)  for i in list(input().split(",")) if i !=" "]
 print(data.mean())
 print(data.median())
 print(data.mode())
+print(data.standered_deviation())
